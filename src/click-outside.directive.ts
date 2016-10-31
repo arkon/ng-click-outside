773d8a2771613e1b76b5f5784199377491696342
Julio War <j.guerra@unadeca.net>
2016-10-31 14:22:02 -0600

    feature to exclude certain parts of all DOM with querySelector.

--- src/click-outside.directive.ts
+++ src/click-outside.directive.ts
@@ -1,67 +1,89 @@
 import {
-  Directive,
-  ElementRef,
-  EventEmitter,
-  Inject,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  Output,
-  SimpleChanges
+    Directive,
+    ElementRef,
+    EventEmitter,
+    Inject,
+    Input,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    Output,
+    SimpleChanges
 } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 
 @Directive({ selector: '[clickOutside]' })
 export class ClickOutsideDirective implements OnInit, OnDestroy, OnChanges {
-  @Input() attachOutsideOnClick: boolean = false;
-  @Output() clickOutside: EventEmitter<Event> = new EventEmitter<Event>();
+    @Input() attachOutsideOnClick: boolean = false;
+    @Input() exclude:string = "";
+    private nodeExcluded = [];
+    @Output() clickOutside: EventEmitter<Event> = new EventEmitter<Event>();
 
-  constructor(
-    @Inject(DOCUMENT) private _document /*: HTMLDocument*/,
-    private _el: ElementRef) {
-    this._initOnClickBody = this._initOnClickBody.bind(this);
-    this._onClickBody = this._onClickBody.bind(this);
-  }
-
-  ngOnInit() {
-    this._init();
-  }
-
-  ngOnDestroy() {
-    if (this.attachOutsideOnClick) {
-      this._el.nativeElement.removeEventListener('click', this._initOnClickBody);
+    constructor(
+        @Inject(DOCUMENT) private _document /*: HTMLDocument*/,
+        private _el: ElementRef) {
+        this._initOnClickBody = this._initOnClickBody.bind(this);
+        this._onClickBody = this._onClickBody.bind(this);
     }
 
-    this._document.body.removeEventListener('click', this._onClickBody);
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['attachOutsideOnClick'] &&
-      changes['attachOutsideOnClick'].previousValue !== changes['attachOutsideOnClick'].currentValue) {
-      this._init();
+    ngOnInit() {
+        this._init();
     }
-  }
 
-  private _init() {
-    if (this.attachOutsideOnClick) {
-      this._el.nativeElement.addEventListener('click', this._initOnClickBody);
-    } else {
-      this._initOnClickBody();
-    }
-  }
+    ngOnDestroy() {
+        if (this.attachOutsideOnClick) {
+            this._el.nativeElement.removeEventListener('click', this._initOnClickBody);
+        }
 
-  private _initOnClickBody() {
-    this._document.body.addEventListener('click', this._onClickBody);
-  }
-
-  private _onClickBody(e: Event) {
-    if (!this._el.nativeElement.contains(e.target)) {
-      this.clickOutside.emit(e);
-
-      if (this.attachOutsideOnClick) {
         this._document.body.removeEventListener('click', this._onClickBody);
-      }
     }
-  }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['attachOutsideOnClick'] &&
+            changes['attachOutsideOnClick'].previousValue !== changes['attachOutsideOnClick'].currentValue) {
+            this._init();
+        }
+    }
+
+    private _init() {
+
+        let nodeExcluded = this.exclude.split(',');
+
+        nodeExcluded.forEach((selector) => {
+            let node = this._document.querySelector(selector);
+            if(node != null) this.nodeExcluded.push(node);
+        });
+
+        if (this.attachOutsideOnClick) {
+            this._el.nativeElement.addEventListener('click', this._initOnClickBody);
+        } else {
+            this._initOnClickBody();
+        }
+    }
+
+    private _initOnClickBody() {
+        this._document.body.addEventListener('click', this._onClickBody);
+    }
+
+    private _onClickBody(e: Event) {
+        if (!this._el.nativeElement.contains(e.target) && !this.isExclude(e.target)) {
+            this.clickOutside.emit(e);
+
+            if (this.attachOutsideOnClick) {
+                this._document.body.removeEventListener('click', this._onClickBody);
+            }
+        }
+    }
+
+    private isExclude(target):boolean {
+        var flag = false;
+        this.nodeExcluded.forEach((node) => {
+            if(node.contains(target) || target == node) {
+                flag = true;
+                return ;
+            }
+
+        });
+        return flag;
+    }
 }

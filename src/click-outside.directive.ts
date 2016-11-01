3c008450d49e8a09528d2940ba58d5e501155e76
Julio War <j.guerra@unadeca.net>
2016-10-31 21:15:42 -0600

    checking changes with exclude variable

--- src/click-outside.directive.ts
+++ src/click-outside.directive.ts
@@ -1,89 +1,97 @@
 import {
-    Directive,
-    ElementRef,
-    EventEmitter,
-    Inject,
-    Input,
-    OnChanges,
-    OnDestroy,
-    OnInit,
-    Output,
-    SimpleChanges
+  Directive,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges
 } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import {DOCUMENT} from '@angular/platform-browser';
 
-@Directive({ selector: '[clickOutside]' })
+@Directive({selector: '[clickOutside]'})
 export class ClickOutsideDirective implements OnInit, OnDestroy, OnChanges {
-    @Input() attachOutsideOnClick: boolean = false;
-    @Input() exclude:string = "";
-    private nodeExcluded = [];
-    @Output() clickOutside: EventEmitter<Event> = new EventEmitter<Event>();
+  @Input() attachOutsideOnClick: boolean = false;
+  @Input() exclude: string = " ";
+  private nodeExcluded = [];
+  @Output() clickOutside: EventEmitter<Event> = new EventEmitter<Event>();
 
-    constructor(
-        @Inject(DOCUMENT) private _document /*: HTMLDocument*/,
-        private _el: ElementRef) {
-        this._initOnClickBody = this._initOnClickBody.bind(this);
-        this._onClickBody = this._onClickBody.bind(this);
+  constructor(@Inject(DOCUMENT) private _document /*: HTMLDocument*/,
+              private _el: ElementRef) {
+    this._initOnClickBody = this._initOnClickBody.bind(this);
+    this._onClickBody = this._onClickBody.bind(this);
+  }
+
+  ngOnInit() {
+    this._init();
+  }
+
+  ngOnDestroy() {
+    if (this.attachOutsideOnClick) {
+      this._el.nativeElement.removeEventListener('click', this._initOnClickBody);
     }
 
-    ngOnInit() {
-        this._init();
+    this._document.body.removeEventListener('click', this._onClickBody);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ( this.validateChanges(changes['attachOutsideOnClick']) ||
+         this.validateChanges(changes['exclude'])) {
+      this._init();
     }
+  }
 
-    ngOnDestroy() {
-        if (this.attachOutsideOnClick) {
-            this._el.nativeElement.removeEventListener('click', this._initOnClickBody);
-        }
+  validateChanges(object:any): boolean {
+    return (object && object.previousValue !== object.currentValue)
+  }
 
-        this._document.body.removeEventListener('click', this._onClickBody);
-    }
-
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes['attachOutsideOnClick'] &&
-            changes['attachOutsideOnClick'].previousValue !== changes['attachOutsideOnClick'].currentValue) {
-            this._init();
-        }
-    }
-
-    private _init() {
-
-        let nodeExcluded = this.exclude.split(',');
-
-        nodeExcluded.forEach((selector) => {
+  private _init() {
+    if (this.exclude) {
+      this.exclude.split(',').forEach((selector) => {
+        if (selector) {
+          try {
             let node = this._document.querySelector(selector);
-            if(node != null) this.nodeExcluded.push(node);
-        });
-
-        if (this.attachOutsideOnClick) {
-            this._el.nativeElement.addEventListener('click', this._initOnClickBody);
-        } else {
-            this._initOnClickBody();
+            if (node) this.nodeExcluded.push(node);
+          } catch (error) {
+            console.error("Check your selector syntax.")
+          }
         }
+      });
     }
 
-    private _initOnClickBody() {
-        this._document.body.addEventListener('click', this._onClickBody);
+    if (this.attachOutsideOnClick) {
+      this._el.nativeElement.addEventListener('click', this._initOnClickBody);
+    } else {
+      this._initOnClickBody();
     }
+  }
 
-    private _onClickBody(e: Event) {
-        if (!this._el.nativeElement.contains(e.target) && !this.isExclude(e.target)) {
-            this.clickOutside.emit(e);
+  private _initOnClickBody() {
+    this._document.body.addEventListener('click', this._onClickBody);
+  }
 
-            if (this.attachOutsideOnClick) {
-                this._document.body.removeEventListener('click', this._onClickBody);
-            }
-        }
+  private _onClickBody(e: Event) {
+    if (!this._el.nativeElement.contains(e.target) && !this.isExclude(e.target)) {
+      this.clickOutside.emit(e);
+
+      if (this.attachOutsideOnClick) {
+        this._document.body.removeEventListener('click', this._onClickBody);
+      }
     }
+  }
 
-    private isExclude(target):boolean {
-        var flag = false;
-        this.nodeExcluded.forEach((node) => {
-            if(node.contains(target) || target == node) {
-                flag = true;
-                return ;
-            }
+  private isExclude(target): boolean {
+    var flag = false;
 
-        });
-        return flag;
+    for (let c = 0; c < this.nodeExcluded.length; c++) {
+      if (this.nodeExcluded[c].contains(target) || target == this.nodeExcluded[c]) {
+        flag = true;
+        break;
+      }
     }
+    return flag;
+  }
 }

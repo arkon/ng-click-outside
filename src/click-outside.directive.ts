b3b30ae5ff4e6c38ac56124577b018a94eec4f78
Julio Guerra <julio.war017@gmail.com>
2016-11-16 19:40:52 -0600

    Feature to exclude certain parts of DOM.  (#5)
    * feature to exclude certain parts of all DOM with querySelector.
    
    * checking changes with exclude variable

--- src/click-outside.directive.ts
+++ src/click-outside.directive.ts
@@ -10,16 +10,17 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import {DOCUMENT} from '@angular/platform-browser';
 
-@Directive({ selector: '[clickOutside]' })
+@Directive({selector: '[clickOutside]'})
 export class ClickOutsideDirective implements OnInit, OnDestroy, OnChanges {
   @Input() attachOutsideOnClick: boolean = false;
+  @Input() exclude: string = " ";
+  private nodeExcluded = [];
   @Output() clickOutside: EventEmitter<Event> = new EventEmitter<Event>();
 
-  constructor(
-    @Inject(DOCUMENT) private _document /*: HTMLDocument*/,
-    private _el: ElementRef) {
+  constructor(@Inject(DOCUMENT) private _document /*: HTMLDocument*/,
+              private _el: ElementRef) {
     this._initOnClickBody = this._initOnClickBody.bind(this);
     this._onClickBody = this._onClickBody.bind(this);
   }
@@ -37,14 +38,30 @@ export class ClickOutsideDirective implements OnInit, OnDestroy, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['attachOutsideOnClick'] &&
-      changes['attachOutsideOnClick'].previousValue !== changes['attachOutsideOnClick'].currentValue) {
+    if ( this.validateChanges(changes['attachOutsideOnClick']) ||
+         this.validateChanges(changes['exclude'])) {
       this._init();
     }
   }
 
-  /** @internal */
+  validateChanges(object:any): boolean {
+    return (object && object.previousValue !== object.currentValue)
+  }
+
   private _init() {
+    if (this.exclude) {
+      this.exclude.split(',').forEach((selector) => {
+        if (selector) {
+          try {
+            let node = this._document.querySelector(selector);
+            if (node) this.nodeExcluded.push(node);
+          } catch (error) {
+            console.error("Check your selector syntax.")
+          }
+        }
+      });
+    }
+
     if (this.attachOutsideOnClick) {
       this._el.nativeElement.addEventListener('click', this._initOnClickBody);
     } else {
@@ -59,12 +76,24 @@ export class ClickOutsideDirective implements OnInit, OnDestroy, OnChanges {
 
   /** @internal */
   private _onClickBody(e: Event) {
-    if (!this._el.nativeElement.contains(e.target)) {
+    if (!this._el.nativeElement.contains(e.target) && !this.isExclude(e.target)) {
       this.clickOutside.emit(e);
 
       if (this.attachOutsideOnClick) {
         this._document.body.removeEventListener('click', this._onClickBody);
       }
     }
+  }
+
+  private isExclude(target): boolean {
+    var flag = false;
+
+    for (let c = 0; c < this.nodeExcluded.length; c++) {
+      if (this.nodeExcluded[c].contains(target) || target == this.nodeExcluded[c]) {
+        flag = true;
+        break;
+      }
+    }
+    return flag;
   }
 }
